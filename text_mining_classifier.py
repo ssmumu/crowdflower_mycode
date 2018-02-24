dd2e35858db8284663b17f67c419951357d10e0d
ssmumu <ssmumu@ssmumus-MacBook-Pro.local>
2018-02-24 09:19:57 -0500

    SVM done

--- text_mining_classifier.py
+++ text_mining_classifier.py
@@ -11,43 +11,33 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 import re
 from numpy import array
+from sklearn.linear_model import SGDClassifier
 
-df = pd.read_csv("text_emotion.csv")
-feature_column_names = ['content_modified']
+df = pd.read_csv("text_sentiment_copywhole.csv", low_memory=False)
+feature_column_names = ['content']
+# feature_column_names = ['content_modified']
 predicted_class_name = ['sentiment']
-X = df[feature_column_names].values
-y = df[predicted_class_name].values
+X = df[feature_column_names].values.astype(str)
+y = df[predicted_class_name].values.astype(str)
 
-# regex = re.compile('[^a-zA-Z0-9 ]')
-# X = [[regex.sub('',(''.join(str(x))))] for x in X]
-# X = array(X)
 
 split_test_size = 0.3
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split_test_size, random_state = 42)
-print("{0:0.2f}% in training set".format((len(X_train)/len(df.index)) * 100))
-print("{0:0.2f}% in test set".format((len(X_test)/len(df.index)) * 100))
-
-# count_vect = CountVectorizer()
-# X_train_counts = count_vect.fit_transform(X_train.ravel())
-# print(X_train_counts.shape)
-
-# tfidf_transformer = TfidfTransformer()
-# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-# print(X_train_tfidf.shape)
+print "{0:0.2f}% in training set".format((len(X_train)/float(len(df.index))) * 100)
+print "{0:0.2f}% in test set".format((len(X_test)/float(len(df.index))) * 100)
 
 
-# clf = MultinomialNB().fit(X_train_tfidf, y_train.ravel())
-
+def buildModel(clf, modelName):
+	print "\n###",modelName,"###"
+	clf = clf.fit(X_train.ravel(), y_train.ravel())
+	predicted_train = clf.predict(X_train.ravel())
+	print "Train Accuracy: ",(np.mean(predicted_train == y_train.ravel()))*100, "%"
+	predicted_test = clf.predict(X_test.ravel())
+	print "Test Accuracy: ",(np.mean(predicted_test == y_test.ravel()))*100, "%"
 
 
 text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])
-text_clf = text_clf.fit(X_train.ravel(), y_train.ravel())
-predicted = text_clf.predict(X_test.ravel())
-print( "MultinomialNB accuracy: ",(np.mean(predicted == y_test.ravel()))*100, "%")
+buildModel(text_clf, "Multinomial Naive Bayes")
 
-nb_model = GaussianNB()
-nb_model.fit(X_train, y_train.ravel())
-prediction_from_test_data = nb_model.predict(X_test)
-accuracy = metrics.accuracy_score(y_test.ravel(), prediction_from_test_data)
-print("GaussianNB accuracy: ",accuracy*100,"%")
-
+text_clf_svm = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),('clf-svm', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)),])
+buildModel(text_clf_svm, "SVM")
